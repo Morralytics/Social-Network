13933c8fc873cec5490ffae38ce2eeb2ec499efd
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-10 17:28:34 -0800

    Added basic CRUD functions that are being exported.

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+
+module.exports = {
+    getUsers(req, res) {
+
+    },
+    getSingleUser(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+};
