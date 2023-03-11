fca3a7873eb4f971fe9b9babea23802078157ce7
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-10 16:50:11 -0800

    Added schema virtual.

--- models/User.js
+++ models/User.js
@@ -2,16 +2,16 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        username: { 
-            type: String, 
-            unique: true, 
-            required: true, 
-            trim: true 
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
         },
-        email: { 
-            type: String, 
-            unique: true, 
-            required: true, 
+        email: {
+            type: String,
+            unique: true,
+            required: true,
             match: /.+\@.+\..+/,
         },
         thoughts: [
@@ -34,3 +34,9 @@ const userSchema = new Schema(
         id: false,
     },
 );
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
