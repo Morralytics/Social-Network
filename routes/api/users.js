97397c0e95b63aae1cd8a8b1bc9f459469fd29ba
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-10 18:13:40 -0800

    Added the routes by chaining the get, post, update, and delete.

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,3 +7,9 @@ const {
     updateUser,
     deleteUser,
 } = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).update(updateUser).delete(deleteUser);
+
+module.exports = router;
