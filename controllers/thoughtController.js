fb7bce21fc93e20606f1c4fd095d91923df745fd
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-11 21:32:44 -0800

    Finished get single thought function that returns JSON data for a single thought based on its ID.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,7 +7,12 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
     getSingleThought(req, res) {
-
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(400).json({ message: 'There is no thought with that asssociated ID.' })
+                    : res.json(thought))
+            .catch((err) => res.status(500).json(err));
     },
     createThought(req, res) {
 
