0ab00ada5285bbd22ca82ed723bfb9bde574988d
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-11 21:29:06 -0800

    Finished get thought function that returns JSON data for all thoughts.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,7 +2,9 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     getThoughts(req, res) {
-
+        Thought.find()
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
     },
     getSingleThought(req, res) {
 
